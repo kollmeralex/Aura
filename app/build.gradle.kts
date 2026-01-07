26bb1137309863c3d80a290200bff1664fdc52db
Hevend <hevend.hussein@stud.uni-hannover.de>
2026-01-07 17:57:28 +0100

    Update dependency to v1.1.1 with CouchDB query fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("com.github.kollmeralex:Aura:v1.1.0")
+    implementation("com.github.kollmeralex:Aura:v1.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
